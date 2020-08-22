f9859dec61ae37e553dce2afc70ceedd3581009f
Federico Aloi <federico.aloi@gmail.com>
2020-08-22 11:42:44 -0300

    Initial commit

---
+++ src/test/kotlin/ar/edu/unahur/obj2/vendedores/VendedorTest.kt
@@ -0,0 +1,39 @@
+package ar.edu.unahur.obj2.vendedores
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+
+class VendedorTest : DescribeSpec({
+  val misiones = Provincia(1300000)
+  val sanIgnacio = Ciudad(misiones)
+
+  describe("Vendedor fijo") {
+    val obera = Ciudad(misiones)
+    val vendedorFijo = VendedorFijo(obera)
+
+    describe("puedeTrabajarEn") {
+      it("su ciudad de origen") {
+        vendedorFijo.puedeTrabajarEn(obera).shouldBeTrue()
+      }
+      it("otra ciudad") {
+        vendedorFijo.puedeTrabajarEn(sanIgnacio).shouldBeFalse()
+      }
+    }
+  }
+
+  describe("Viajante") {
+    val cordoba = Provincia(2000000)
+    val villaDolores = Ciudad(cordoba)
+    val viajante = Viajante(listOf(misiones))
+
+    describe("puedeTrabajarEn") {
+      it("una ciudad que pertenece a una provincia habilitada") {
+        viajante.puedeTrabajarEn(sanIgnacio).shouldBeTrue()
+      }
+      it("una ciudad que no pertenece a una provincia habilitada") {
+        viajante.puedeTrabajarEn(villaDolores).shouldBeFalse()
+      }
+    }
+  }
+})
