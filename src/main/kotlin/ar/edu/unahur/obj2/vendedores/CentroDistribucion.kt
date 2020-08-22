6797c39af5354ff070ecbc7b7d8d24c8904608ca
tomastoloza <tomasezequieltoloza@gmail.com>
2020-08-22 16:25:17 -0300

    Add missing vendedor impl and CentroDistribucion class

---
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDistribucion.kt
@@ -0,0 +1,54 @@
+package ar.edu.unahur.obj2.vendedores
+
+import java.lang.Exception
+
+class CentroDistribucion(val ciudad: Ciudad) {
+    private val vendedores = mutableListOf<Vendedor>()
+    fun agregarVendedor(vendedor: Vendedor) {
+        if (this.vendedores.contains(vendedor)) {
+            throw Exception("Vendedor ya existe")
+        } else {
+            this.vendedores.add(vendedor)
+        }
+    }
+
+    // el vendedor estrella, que es el que tiene mayor puntaje total por certificaciones.
+    fun getVendedorEstrella(): Vendedor? {
+        var maxPuntaje = 0
+        var vendedorEstrella: Vendedor? = null
+        this.vendedores.forEach { it ->
+            if (it.puntajeCertificaciones() > maxPuntaje) {
+                maxPuntaje = it.puntajeCertificaciones()
+                vendedorEstrella = it
+            }
+        }
+        return vendedorEstrella
+    }
+
+    // si puede cubrir, o no, una ciudad dada. La condición es que al menos uno de los vendedores registrados pueda trabajar en esa ciudad.
+    fun puedeCubrir(ciudad: Ciudad): Boolean {
+        for (vendedor in this.vendedores) {
+            if (vendedor.puedeTrabajarEn(ciudad)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    //la colección de vendedores genéricos registrados. Un vendedor se considera genérico si tiene al menos una certificación que no es de productos.
+    fun getVendedoresGenericos(ciudad: Ciudad): MutableList<Vendedor> {
+        val vendedoresGenericos = mutableListOf<Vendedor>()
+        for (vendedor in this.vendedores) {
+            if (vendedor.otrasCertificaciones() > 0) {
+                vendedoresGenericos.add(vendedor)
+            }
+        }
+        return vendedoresGenericos
+    }
+
+    //si es robusto, la condición es que al menos 3 de sus vendedores registrados sea firme.
+    fun esRobusto(): Boolean {
+        return this.vendedores.count { vendedor -> vendedor.esFirme() } >= 3
+    }
+
+}
--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Vendedor.kt
@@ -3,49 +3,68 @@ package ar.edu.unahur.obj2.vendedores
 class Certificacion(val esDeProducto: Boolean, val puntaje: Int)
 
 abstract class Vendedor {
-  // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
-  // Además, a una MutableList se le pueden agregar elementos
-  val certificaciones = mutableListOf<Certificacion>()
+    // Acá es obligatorio poner el tipo de la lista, porque como está vacía no lo puede inferir.
+    // Además, a una MutableList se le pueden agregar elementos
+    private val certificaciones = mutableListOf<Certificacion>()
 
-  // Definimos el método abstracto.
-  // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
-  abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
+    // Definimos el método abstracto.
+    // Como no vamos a implementarlo acá, es necesario explicitar qué devuelve.
+    abstract fun puedeTrabajarEn(ciudad: Ciudad): Boolean
 
-  // En las funciones declaradas con = no es necesario explicitar el tipo
-  fun esVersatil() =
-    certificaciones.size >= 3
-      && this.certificacionesDeProducto() >= 1
-      && this.otrasCertificaciones() >= 1
+    // En las funciones declaradas con = no es necesario explicitar el tipo
+    fun esVersatil() =
+            certificaciones.size >= 3
+                    && this.certificacionesDeProducto() >= 1
+                    && this.otrasCertificaciones() >= 1
 
-  // Si el tipo no está declarado y la función no devuelve nada, se asume Unit (es decir, vacío)
-  fun agregarCertificacion(certificacion: Certificacion) {
-    certificaciones.add(certificacion)
-  }
+    // Si el tipo no está declarado y la función no devuelve nada, se asume Unit (es decir, vacío)
+    fun agregarCertificacion(certificacion: Certificacion) {
+        certificaciones.add(certificacion)
+    }
 
-  fun esFirme() = this.puntajeCertificaciones() >= 30
+    fun esFirme() = this.puntajeCertificaciones() >= 30
 
-  fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
-  fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
+    fun certificacionesDeProducto() = certificaciones.count { it.esDeProducto }
+    fun otrasCertificaciones() = certificaciones.count { !it.esDeProducto }
 
-  fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
+    fun puntajeCertificaciones() = certificaciones.sumBy { c -> c.puntaje }
+
+    abstract fun esInfluyente(): Boolean
 }
 
 // En los parámetros, es obligatorio poner el tipo
 class VendedorFijo(val ciudadOrigen: Ciudad) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return ciudad == ciudadOrigen
-  }
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
+        return ciudad == ciudadOrigen
+    }
+
+    override fun esInfluyente() = false
+
+    fun getCiudadEnLaQueVive() = this.ciudadOrigen
 }
+
 
 // A este tipo de List no se le pueden agregar elementos una vez definida
 class Viajante(val provinciasHabilitadas: List<Provincia>) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return provinciasHabilitadas.contains(ciudad.provincia)
-  }
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
+        return provinciasHabilitadas.contains(ciudad.provincia)
+    }
+
+    //    viajante: la población total sumando todas las provincias donde está habilitado, debe ser de 10 millones o superior.
+    override fun esInfluyente(): Boolean {
+        return provinciasHabilitadas.sumBy { provincia -> provincia.poblacion } > 10000000
+    }
 }
 
 class ComercioCorresponsal(val ciudades: List<Ciudad>) : Vendedor() {
-  override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
-    return ciudades.contains(ciudad)
-  }
+    override fun puedeTrabajarEn(ciudad: Ciudad): Boolean {
+        return ciudades.contains(ciudad)
+    }
+
+    //comercio corresponsal: debe tener sucursales en al menos 5 ciudades, o bien en al menos 3 provincias considerando la provincia de cada ciudad donde tiene sucursal.
+    override fun esInfluyente(): Boolean {
+        val provincias = this.ciudades.filter { true }
+        return this.ciudades.size > 5 || provincias.size > 3
+
+    }
 }
